2783cc115ec3fb900c0fcf52e87e7f57acdf3a3d
미치고 싶은 개발자. <seojunho22@gmail.com>
2025-03-26 14:51:10 +0900

    물건 임대 구매 서비스 프로젝트 생성

---
+++ src/main/java/com/needthis/needthis/NeedthisApplication.java
@@ -0,0 +1,13 @@
+package com.needthis.needthis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NeedthisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NeedthisApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/needthis/needthis/NeedthisApplicationTests.java
@@ -0,0 +1,13 @@
+package com.needthis.needthis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NeedthisApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
